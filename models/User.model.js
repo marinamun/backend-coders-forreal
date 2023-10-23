b2e541a09aa56b068b8a18e1f16980b7e670372d
Marina Muñoz <marinacerejidoo@gmail.com>
2023-10-23 16:42:19 +0200

    User model

--- models/User.model.js
+++ models/User.model.js
@@ -3,21 +3,39 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    email: {
+    username: {
       type: String,
-      required: [true, 'Email is required.'],
+      required: [true, "Username is required."],
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     password: {
       type: String,
-      required: [true, 'Password is required.']
-    }
+      required: [true, "Password is required."],
+    },
+    languages: {
+      type: [String],
+      enum: ["JavaScript", "Python", "Java", "C++", "C#"],
+    },
+    country: { type: String },
+    level: { type: String, enum: ["Learner", "Junior", "Senior"] },
+    photo: {
+      type: String,
+      default:
+        "https://digimedia.web.ua.pt/wp-content/uploads/2017/05/default-user-image.png",
+    },
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
   }
 );
 
