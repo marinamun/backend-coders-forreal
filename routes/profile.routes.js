73ce1c6c0b5f15f3ad2fdd35d1843688aff3e6d3
Telma Irina <telmairinacoelho@gmail.com>
2023-10-25 16:54:04 +0100

    changes 3

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
+const User = require('../models/User.model')
 
-router.get('/userId', async (req, res) => {
+router.get('/:userId', async (req, res) => {
   try {
     const responseFromAPI = await fetch(
-      `http://localhost:5005/api/users/${req.params._id}`
+      `http://localhost:5005/api/users/${req.params.userId}`
     )
     if (responseFromAPI.ok) {
       const userFromAPI = await responseFromAPI.json()
