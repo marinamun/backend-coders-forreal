1a6aa4f13146b515d895ac8f838cbebd27691dc4
Marina Muñoz <marinacerejidoo@gmail.com>
2023-10-25 12:09:38 +0200

    Created question routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,5 +3,7 @@ const router = require("express").Router();
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
+const questionsRoutes = require("./question.routes");
+router.use("/questions", questionsRoutes);
 
 module.exports = router;
