b9a2714483841e0ae8a1f043f8e803045fed4783
Telma Irina <telmairinacoelho@gmail.com>
2023-10-26 15:02:51 +0100

    user changes

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,34 +2,15 @@ const router = require('express').Router()
 const User = require('../models/User.model')
 const mongoose = require("mongoose");
 
-/* router.get('/', async (req, res) => {
-    try {
-        const allUsers = await User.find();
-        res.status(200).json({allUsers});
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error });
-    }
-});
-
-
-router.post('/new', async (req,res) => {
-    try {
-        const newUser = await User.create();
-        res.status(201).json({newUser});
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error });
-    }
-}) */
-
 router.get('/:userId', async (req, res) => {
     const { userId } = req.params;
   if (mongoose.isValidObjectId(userId)) {
       try {
         const oneUser = await User.findById(userId)
         if (oneUser) {
-            res.status(201).json({ user: oneUser });
+            const user = oneUser._doc
+            delete user.password
+            res.status(201).json({ user: user });
         } else {
           res.status(404).json({ message: "user not found" });
         }
