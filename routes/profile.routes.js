9d2757405784bd7a819343bf14be247fb988c3dd
Alice <alicepennec@gmail.com>
2023-10-26 17:36:54 +0200

    profie routes updated

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -21,6 +21,25 @@ router.get('/:userId', async (req, res) => {
     } else {
       res.status(500).json({ message: "id seems wrong" });
     }
+});
+
+router.put('/:userId', async (req, res) => {
+  const { userId } = req.params;
+if (mongoose.isValidObjectId(userId)) {
+    try {
+      const oneUser = await User.findByIdAndUpdate(userId, req.body, {new: true})
+      if (oneUser) {
+          const user = oneUser._doc
+          delete user.password
+          res.status(200).json({ user: oneUser });
+      } 
+    }  catch (error) {
+      console.log(error);
+      res.status(400).json({ error });
+    }
+  } else {
+    res.status(500).json({ message: "id seems wrong" });
+  }
 })
 
 module.exports = router
