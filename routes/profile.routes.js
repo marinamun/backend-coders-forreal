64c91b76ebe16397d68ac184e12fa654bd096b8d
Alice <alicepennec@gmail.com>
2023-10-27 10:52:32 +0200

    delete for user created

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -27,11 +27,11 @@ router.put('/:userId', async (req, res) => {
   const { userId } = req.params;
 if (mongoose.isValidObjectId(userId)) {
     try {
-      const oneUser = await User.findByIdAndUpdate(userId, req.body, {new: true})
-      if (oneUser) {
+      const updatedUser = await User.findByIdAndUpdate(userId, req.body, {new: true})
+      if (updatedUser) {
           const user = oneUser._doc
           delete user.password
-          res.status(200).json({ user: oneUser });
+          res.status(200).json({ user: updatedUser });
       } 
     }  catch (error) {
       console.log(error);
@@ -39,6 +39,16 @@ if (mongoose.isValidObjectId(userId)) {
     }
   } else {
     res.status(500).json({ message: "id seems wrong" });
+  }
+});
+
+router.delete ('/:userId', async (req, res) => {
+  const {userId} = req.params;
+  try {
+    await User.findByIdAndDelete(userId)
+    res.status(204).json({message : 'Profile deleted'})
+  } catch (error) {
+    res.status(500).json(error)
   }
 })
 
