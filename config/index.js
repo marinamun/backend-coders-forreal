f95b771581ef0f9882ae2794c6597c664ebc353a
Marina Muñoz <marinacerejidoo@gmail.com>
2023-11-02 14:42:46 +0100

    update cors url

--- config/index.js
+++ config/index.js
@@ -16,27 +16,27 @@ const cors = require("cors");
 const FRONTEND_URL =
   process.env.ORIGIN ||
   "http://localhost:3000" ||
-  "https://lord-of-the-coders.netlify.app/" ||
+  "https://lord-of-the-coders.netlify.app" ||
   "http://127.0.0.1:5173";
 
-  // Middleware configuration
-  module.exports = (app) => {
-    // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
-    // Services like heroku use something called a proxy and you need to add this to your server
-    app.set("trust proxy", 1);
+// Middleware configuration
+module.exports = (app) => {
+  // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
+  // Services like heroku use something called a proxy and you need to add this to your server
+  app.set("trust proxy", 1);
 
-    // controls a very specific header to pass headers from the frontend
-    app.use(
-      cors({
-        origin: [FRONTEND_URL],
-      })
-    );
+  // controls a very specific header to pass headers from the frontend
+  app.use(
+    cors({
+      origin: [FRONTEND_URL],
+    })
+  );
 
-    // In development environment the app logs
-    app.use(logger("dev"));
+  // In development environment the app logs
+  app.use(logger("dev"));
 
-    // To have access to `body` property in the request
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(cookieParser());
-  };
+  // To have access to `body` property in the request
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+};
